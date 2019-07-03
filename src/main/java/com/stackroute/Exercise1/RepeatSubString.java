639adb93ac5ed07dc137506379fa17af8eb7943b
meghanakonuganti <51950346+meghanakonuganti@users.noreply.github.com>
2019-07-03 10:42:48 +0530

    Update RepeatSubString.java

--- src/main/java/com/stackroute/Exercise1/RepeatSubString.java
+++ src/main/java/com/stackroute/Exercise1/RepeatSubString.java
@@ -1,6 +1,7 @@
 package com.stackroute.Exercise1;
 
 public class RepeatSubString {
+    //to repeat the given substring
     public static String finalOutputString(String word1,int number1) {
         int len=word1.length();
         String str1=word1.substring(len-number1,len);
